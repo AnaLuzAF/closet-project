b08849ca88ce4a6aef1aa0b4911d87957993c226
cpalaciosv <cpalaciosvallejo@hotmail.com>
2022-05-25 19:02:37 +0200

    FUNCIONA POST ITEMS CON IMAGENES AAAAAA

--- closet-api/src/main/java/org/iesfm/closet/client/ItemsApi.java
+++ closet-api/src/main/java/org/iesfm/closet/client/ItemsApi.java
@@ -6,8 +6,6 @@ import java.util.List;
 
 public interface ItemsApi {
 
-    // crear metodos que va a usar la web
-
     List<ItemRest> listUserItems(int userId, String itemType);
 
     int insert(ItemRest item, int userId);
