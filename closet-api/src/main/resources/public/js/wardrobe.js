ceaa64baca248db25cc8bcdf4fc97aa379899196
cpalaciosv <cpalaciosvallejo@hotmail.com>
2022-05-27 18:51:27 +0200

    cambio wardrobe css y js cambio home para que se muestre al iniciar

--- closet-api/src/main/resources/public/js/wardrobe.js
+++ closet-api/src/main/resources/public/js/wardrobe.js
@@ -31,8 +31,8 @@ function loadWardrobe() {
                <div class="dropdown">
 
 <select class="dropbtn" name="select">
-  <option value="value1">Value 1</option>
-  <option value="value2" selected>Value 2</option>
+  <option value="value1" selected>Value 1</option>
+  <option value="value2">Value 2</option>
   <option value="value3">Value 3</option>
 </select>
 
