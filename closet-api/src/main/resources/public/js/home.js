1ed88a10aac7bbe12277e9a8927c84463a973181
Ana Luz Aviles Florentino <analuzaviles@gmail.com>
2022-05-28 22:28:05 +0200

    cambios en el index.html, y el css

--- closet-api/src/main/resources/public/js/home.js
+++ closet-api/src/main/resources/public/js/home.js
@@ -2,38 +2,40 @@ function loadHome() {
     var homeStructure = $('#main');
     homeStructure.empty();
 
-    var home = `   <div class="inicio">
-                                       <p>Bienvenido a tu Armario Online donde podrás crear tus Outfits en un sólo click</p>
-                                <a href="mailto:analuzaviles@gmail.com" id="trucazo"></a>
+    var home = `<div class="inicio">
+                                    <h1><a href="index.html">Online Closet</a></h1>
+                                    <h2>Plan your <span>best outfits</span> ahead</h2>
                                    </div>
                         <div class="contenedor-form">
                             <div class="toggle">
-                                <span> Crear Cuenta</span>
+                                <span> Log In/Create Account</span>
                             </div>
 
                             <div class="formulario">
-                                <h2>Iniciar Sesión</h2>
+                                <h2>Log In</h2>
                                 <form action="#">
-                                    <input type="text" placeholder="Usuario" required>
-                                    <input type="password" placeholder="Contraseña" required>
-                                    <input type="submit" value="Iniciar Sesión">
+                                    <input type="text" placeholder="User" required>
+                                    <input id="password-view" type="password" placeholder="Password" required>
+                                    <span>show</span>
+                                    <input type="submit" value="Log In">
                                 </form>
                             </div>
 
-                            <div class="formulario">
-                                <h2>Crea tu Cuenta</h2>
+                            <div class="formulario" id="formulario">
+                                <h2>Create your Account</h2>
                                 <form action="" class="form" id="form">
-                                    <input type="text" name="name" placeholder="Usuario" required>
+                                    <input type="text" name="name" placeholder="User" required>
 
-                                    <input type="password" placeholder="Contraseña" required>
+                                    <input id= "password-view-create" type="password" placeholder="Password" required>
+                                    <span>show</span>
 
-                                    <input type="email" name="correoUser" placeholder="Correo Electronico" required>
+                                    <input type="email" name="emailUser" placeholder="Email" required>
 
-                                    <input type="submit" value="Registrarse">
+                                    <input type="submit" value="Check In">
                                 </form>
                             </div>
                             <div class="reset-password">
-                                <a href="#">Olvidé mi Contraseña</a>
+                                <a href="#">I forgot my password</a>
                             </div>
                         </div>
 
@@ -47,6 +49,33 @@ function loadHome() {
              opacity: 'toggle'
          }, "slow");
      });
+
+     document.querySelector('.contenedor-form .formulario span').addEventListener('click',
+     e => {
+        const passwordInput = document.querySelector('#password-view');
+        if(e.target.classList.contains('show')){
+        e.target.classList.remove('show');
+        e.target.textContent='hidden';
+        passwordInput.type='text';
+        }else{
+        e.target.classList.add('show');
+        e.target.textContent='show';
+        passwordInput.type='password';
+        }
+     });
+     document.querySelector('.contenedor-form #formulario span').addEventListener('click',
+          e => {
+             const passwordInput = document.querySelector('#password-view-create');
+             if(e.target.classList.contains('show')){
+             e.target.classList.remove('show');
+             e.target.textContent='hidden';
+             passwordInput.type='text';
+             }else{
+             e.target.classList.add('show');
+             e.target.textContent='show';
+             passwordInput.type='password';
+             }
+          });
 
 }
 
